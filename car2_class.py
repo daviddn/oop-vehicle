7966024eeba6540a75b3bade10ae6d9b77c635c6
David Naim <DNaim@spartaglobal.com>
2019-09-26 16:40:10 +0100

    did the encapsulation of miles in a car

--- car2_class.py
+++ car2_class.py
@@ -7,9 +7,26 @@ class Car2(Vehicle2):
         self.make = make
         self.model = model
         self.plate = plate
+        self._accidents = 0 # Visibility is limited
+        self.__miles = 0 # Visibility and Access is restricted#
 
     def make_sound(self):
         return 'REVVING MY CAR! VROOM!'
 
+    def accelerate(self): # Using encapsulated private method
+        self.__increase_miles()
+        return 'VROOOOMMMM'
+
     def play_music(self, song):
         return 'Banging song this is: ' + song
+
+    def show_miles(self): # Getter
+        return self.__miles
+
+    def set_miles(self, miles): # Setter
+        # Checking some crazy security stuff before setting
+        self.__miles = miles
+        return 'miles set to ' + miles
+
+    def __increase_miles(self): # Encapsulated private method
+        self.__miles += 100
--- run2.py
+++ run2.py
@@ -1,11 +1,26 @@
 from car2_class import *
 
+# print('Creating a simple instance - hence I know how to write a Class and')
+# vehicle_example = Vehicle2(2, 3, 'green')
+#
+# print('Proof of Inheritance')
+# # Car inherits from Vehicle2
+car_example = Car2(2, 4, 'Blue', 'Volvo', 'XC90', 'XOXOX')
+# print(car_example) # Inheriting the __init__
+# print(car_example.accelerate()) # Inheriting the .acclerate()
+#
+# print('Proof of method polymorphism with make_sound')
+# print(vehicle_example.make_sound())
+# print(car_example.make_sound())
 
+print('Playing with encapsulation')
+print(car_example.wheels)
+car_example._accidents = 100
+print(car_example._accidents)
 
-print('Proof of Inheritance')
-print(Car2(2, 4, 'Blue')) # Inheriting the __init__
-print(Car2(2, 4, 'Blue').accelerate()) # Inheriting the .acclerate()
+# print(car_example.__miles()) # This will break - Because it's a private/encapsulation
+print(car_example.show_miles())
 
-print('Proof of method polymorphismm with make_sound')
-print(Vehicle2(2, 4, 'Blue').make_sound())
-print(Car2(2, 4, 'Blue').make_sound())
+# print(car_example.__increase_miles()) # This will break - Method encapsulation
+print(car_example.accelerate())
+print(car_example.show_miles())
